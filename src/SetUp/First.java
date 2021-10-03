423b2f80e81c90c5623c330b09a298ac493ce49a
14432 <eugen.burca@gmail.com>
2021-10-03 12:51:48 -0700

    udating first time

--- src/SetUp/First.java
+++ src/SetUp/First.java
@@ -3,5 +3,9 @@ package SetUp;
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        // this is a new line
+        // I want to create a history
+        // the text doesnt matter
     }
 }
